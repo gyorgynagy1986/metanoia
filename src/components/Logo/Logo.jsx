626aace9b2f57b90bb73143bbbd26b43f5d2374e
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-12-22 18:18:30 +0100

    text update

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./Logo.module.css";
 import Image from "next/image";
-import photo from "../../../public/assets/vector.png";
+import photo from "../../../public/assets/newlogo.png";
 
 const Logo = () => {
   return (
