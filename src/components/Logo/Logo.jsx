608d2cd7067174ab7cc321cc425432a2006a752f
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2023-12-22 18:10:34 +0100

    text update

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import style from "./Logo.module.css";
 import Image from "next/image";
-import photo from "../../../public/assets/top.svg";
+import photo from "../../../public/assets/12.svg";
 
 const Logo = () => {
   return (
